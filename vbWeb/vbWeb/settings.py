8c7671edc49c995ff2cede5466ca570f38ac3a57
Kushagra Sahu <kushagrasahu.ed@gmail.com>
2016-07-15 23:23:51 +0530

    Setting up default database

--- vbWeb/vbWeb/settings.py
+++ vbWeb/vbWeb/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'vbWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'valardownloadis',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'elixir',
-        'PORT': 3306,
-    }
-}
-
-# DATABASES = { 
-#     'default': 
-#         { 
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, ''),
-#         } 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'valardownloadis',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'elixir',
+#         'PORT': 3306,
+#     }
 # }
+
+DATABASES = { 
+    'default': 
+        { 
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        } 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
